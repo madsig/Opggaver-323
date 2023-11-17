47217b9e96c3add9715b8062e759f6c63a98855c
madsig <madsig@getacademy.no>
2023-11-17 12:35:41 +0100

    initial commit

---
+++ KampTipping/KampTipping/Match.cs
@@ -0,0 +1,32 @@
+﻿using System.Dynamic;
+
+namespace KampTipping
+{
+    internal class Match
+    {
+        public int HomeGoals { get; private set; }
+        public int AwayGoals { get; private set; }
+        public bool MatchIsRunning = true;
+
+        private string _bet = "";
+       
+        public void SetBet(string bet)
+        {
+            _bet = bet;
+        }
+
+        public void AddGoal(string side)
+        {
+            if (side == "H") HomeGoals++;
+            else if (side == "B") AwayGoals++;
+        }
+
+        public string GetResult()
+        {
+            var result = HomeGoals == AwayGoals ? "U" : HomeGoals > AwayGoals ? "H" : "B";
+            var isBetCorrect = _bet.Contains(result);
+            var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
+            return isBetCorrectText;
+        }
+    }
+}
---
+++ KampTipping/KampTipping/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace KampTipping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var match = new Match();
+            Console.Write("Gyldig tips: \n" +
+                          " - H, U, B\n" + 
+                          " - halvgardering: HU, HB, UB\n" + 
+                          " - helgardering: HUB\n" + 
+                          "Hva har du tippet for denne kampen? ");
+            match.SetBet(Console.ReadLine());
+            while (match.MatchIsRunning)
+            {
+                Console.Write("Kommandoer: \n" + 
+                              " - H = scoring hjemmelag\n" + 
+                              " - B = scoring bortelag\n" + 
+                              " - X = kampen er ferdig\n" + 
+                              "Angi kommando: ");
+                var command = Console.ReadLine();
+                match.MatchIsRunning = command != "X";
+                match.AddGoal(command);
+                Console.WriteLine($"Stillingen er {match.HomeGoals}-{match.AwayGoals}.");
+            }
+
+            
+            Console.WriteLine($"Du tippet {match.GetResult()}");
+        }
+    }
+}
